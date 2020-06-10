8612ae9cd0d36b9c30a9d82427634649f17af798
nahjh2 <nahjh2@naver.com>
2020-06-10 06:31:44 +0000

    Add docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rsa_prikey_path="/home/echo/rs256.pem"
+rsa_pubkey_path="/home/echo/rs256.pub"
+
+openssl genrsa -out $rsa_prikey_path 4096
+openssl rsa -in $rsa_prikey_path -pubout -outform PEM -out $rsa_pubkey_path
+
+echo "=======MIGRATE DATABASE======="
+
+echo "**********UPGRADE DB**********"
+
+echo "===========ENV===========";
+printenv
+
+echo "==============================";
+exec "$@"
