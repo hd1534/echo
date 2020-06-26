17e57bbac423c659ffbfb9cdfbf93613f628e90f
nahjh2 <nahjh2@google.com>
2020-06-26 09:01:44 +0900

    fix msql/index.js

--- echo/api/mysql/index.js
+++ echo/api/mysql/index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const { EchoTags } = require("../../models/sql");
+const { Tags } = require("../../models/sql");
 
 router.get("/", (req, res) => {
-  const tags = EchoTags.findAll({ attributes: ["idx", "name"] });
+  const tags = Tags.findAll({ attributes: ["idx", "name"] });
   tags.then((result) => {
     if (!result) {
       res.send("ERRORRRRRRR");
