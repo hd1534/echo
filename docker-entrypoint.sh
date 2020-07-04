11692cd6529811b639dee1a4d1bf0628a1d8f724
nahjh2 <nahjh2@naver.com>
2020-07-04 12:23:54 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,9 @@ rsa_pubkey_path="/home/echo/echo/rs256.pub"
 openssl genrsa -out $rsa_prikey_path 4096
 openssl rsa -in $rsa_prikey_path -pubout -outform PEM -out $rsa_pubkey_path
 
-echo "=======MIGRATE DATABASE======="
-
-echo "**********UPGRADE DB**********"
-
 echo "===========ENV===========";
 
 printenv
 
-echo "==============================";
+echo "===========ENV===========";
 exec "$@"
