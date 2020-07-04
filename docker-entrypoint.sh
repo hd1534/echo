df3f9c9d2ececa1abbdf62868c1861c39d50ee37
nahjh2 <nahjh2@naver.com>
2020-07-04 12:22:13 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rsa_prikey_path="/home/echo/rs256.pem"
-rsa_pubkey_path="/home/echo/rs256.pub"
+rsa_prikey_path="/home/echo/echo/rs256.pem"
+rsa_pubkey_path="/home/echo/echo/rs256.pub"
 
 openssl genrsa -out $rsa_prikey_path 4096
 openssl rsa -in $rsa_prikey_path -pubout -outform PEM -out $rsa_pubkey_path
